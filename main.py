7231858fd586332e0b5745e9dafa2cf079b04678
tanluuuuuuu <20521591@gm.uit.edu.vn>
2022-11-03 10:22:20 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,139 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import seaborn as sns
+from utils import transform_column_to_onehot, split_data_and_train_model, transform_data_to_predict, score_model, train_with_kFold
+from utils import split_column_by_type, label_output_feature, get_confusion_matrix
+
+st.set_page_config(page_title='Linear Regression')
+
+def resetSessionState():
+    st.session_state['trained'] = False 
+    st.session_state['score'] = [0, 0]
+    return
+
+if __name__=='__main__':
+    if 'trained' not in st.session_state:
+        st.session_state['trained'] = False
+        st.session_state['score'] = [0, 0]
+        st.session_state['model'] = None
+        
+    st.title("Classify anything with Logistic Regression")
+    
+    uploaded_file = st.file_uploader("Let's upload your dataset")
+    if uploaded_file is not None:
+        if (uploaded_file.name[-3:] != 'csv'):
+            st.warning("Please up load file csv")
+            st.stop()
+        try:
+            df = pd.read_csv(uploaded_file)
+            st.write(df)
+            column_labels = df.columns.to_numpy()
+            categorical_column_labels, numerical_column_labels = split_column_by_type(df, column_labels)
+            
+            st.header("Select output feature")
+            st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+            selected_output_feature = st.radio("", categorical_column_labels, label_visibility='collapsed')
+            st.write(f"Output feature: {selected_output_feature}")
+
+            st.header("Select training features")
+            cols = st.columns(4)
+            selected_columns = []
+            ii = 0
+            for (i, column_label) in enumerate(column_labels):
+                if (column_label != selected_output_feature):
+                    t = cols[(i - ii) % 4].checkbox(column_label, on_change=resetSessionState)
+                    if t:
+                        selected_columns.append(column_label)
+                else:
+                    ii = 1
+        
+            if (len(selected_columns) == 0):
+                st.info("Select training feature to continue")
+                st.stop()
+        
+            y, label_encoder = label_output_feature(df[selected_output_feature].to_numpy())
+            X = np.array([])
+            dict_encoder = {}
+            cfs_matrix = [[]]
+            X, dict_encoder= transform_column_to_onehot(df=df, selected_columns=selected_columns, dict_encoder=dict_encoder)
+
+            st.header("Train test split: ")
+            kFold_mode = st.checkbox("K Fold", on_change=resetSessionState)
+                    
+            if kFold_mode:
+                n_splits = int(st.number_input("Enter n split: ", min_value=2, step=1))
+                if st.button("Train model!"):
+                    st.session_state['trained'] = True
+                    model, train_score, test_score = train_with_kFold(X, y, n_splits)
+                    st.session_state['score'] = [train_score, test_score]
+                    st.session_state['model'] = model
+                    cfs_matrix = get_confusion_matrix(model, X, y)
+            else:
+                train_split_ratio = float(st.slider('Select train ratio in range', 0, 90, 80, on_change=resetSessionState))
+                if st.button("Train model!"):
+                    st.session_state['trained'] = True
+                    model, X_train, X_test, y_train, y_test = split_data_and_train_model(X=X, y=y, train_split_ratio=train_split_ratio)
+                    st.session_state['model'] = model
+                    test_score = score_model(model, X_test, y_test)
+                    train_score = score_model(model, X_train, y_train)
+                    st.session_state['score'] = [train_score, test_score]
+                    cfs_matrix = get_confusion_matrix(model, X, y)
+
+            col1, col2 = st.columns(2)
+            fig, ax = plt.subplots()
+            if kFold_mode:
+                if not st.session_state['trained']:
+                    st.session_state['score'] = [[0], [0]]
+                r = np.arange(len(st.session_state['score'][0]))
+                ax.bar(r - 0.2, st.session_state['score'][0], color ='b', width = 0.4, label='Train')
+                ax.bar(r + 0.2, st.session_state['score'][1], color ='g', width = 0.4, label='Test')
+                ax.set_xticks(r, r)
+                ax.set_xlabel("Fold")
+                ax.set_ylabel("MSE")
+                ax.set_title("Train test score")
+                ax.legend()
+                col1.pyplot(fig)
+            else:
+                ax.bar(["Train", "Test"], st.session_state['score'], color ='maroon', width = 0.4)
+                ax.set_xlabel("MSE")
+                ax.set_ylabel("Set")
+                ax.set_title("Train test score")
+                col1.pyplot(fig)                
+                
+            # fig2 = sns.heatmap(cfs_matrix)
+            # col2.pyplot(fig2)  
+            df = pd.DataFrame(cfs_matrix, columns=label_encoder.classes_, index=label_encoder.classes_)
+            col2.write(df)
+
+            if not st.session_state['trained']:
+                st.info("Train model to continue")
+                st.stop()
+                
+            st.header("Make Prediction")
+            dict_data_to_prediction = {}
+            for (index, data_column) in enumerate(selected_columns):
+                if (df.dtypes[data_column] == 'object'):
+                    unique_val = df[data_column].unique()
+                    cat_val = st.selectbox(data_column, (unique_val))
+                    cat_val_onehot = dict_encoder[data_column].transform([[cat_val]]).toarray().squeeze()
+                    dict_data_to_prediction[data_column] = cat_val_onehot
+                else:
+                    dict_data_to_prediction[data_column] = st.number_input(data_column)
+            
+            if st.button("Predict!"):
+                print("here")
+                data = transform_data_to_predict(selected_columns, dict_data_to_prediction)
+                try:
+                    model = st.session_state['model'] 
+                    prediction = model.predict(data)
+                    st.write(f"{selected_output_feature}: {label_encoder.inverse_transform([prediction.item(0)]).item(0)}")
+                except Exception as e:
+                    st.write(e)
+            
+        except Exception as e:
+            st.write(e)
---
+++ utils.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+from sklearn import preprocessing
+from sklearn.metrics import log_loss, confusion_matrix
+
+def split_column_by_type(df, column_labels):
+    categorical_column_labels = []
+    numerical_column_labels = []
+    
+    for column_label in column_labels:
+        if (df.dtypes[column_label] == 'object'):
+            categorical_column_labels.append(column_label)
+        else:
+            numerical_column_labels.append(column_label)
+    return categorical_column_labels, numerical_column_labels
+
+def transform_column_to_onehot(df, selected_columns, dict_encoder ):
+    X = np.array([])
+    for column_label in selected_columns:
+        col = df[column_label].to_numpy().reshape(-1, 1)
+        if (df.dtypes[column_label] == 'object'):
+            enc = OneHotEncoder(handle_unknown='ignore')
+            enc = enc.fit(col)
+            dict_encoder[column_label] = enc
+            col = enc.transform(col).toarray()
+        if len(X) == 0:
+            X = col
+        else:
+            X = np.concatenate((X, col), axis=1)
+    return X, dict_encoder
+
+def label_output_feature(y):
+    le = preprocessing.LabelEncoder()
+    le.fit(y)
+    yhat = le.transform(y)
+    return yhat, le
+
+def split_data_and_train_model(X, y, train_split_ratio):
+    model = LogisticRegression()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_split_ratio / 100.0, random_state=42)
+    model.fit(X_train, y_train)
+    return model, X_train, X_test, y_train, y_test
+
+def transform_data_to_predict(selected_columns, dict_data_to_prediction):
+    data_to_prediction = np.array([])
+    for data_column in selected_columns:
+        data_to_prediction = np.append(data_to_prediction, dict_data_to_prediction[data_column])
+    data = np.array(data_to_prediction).reshape(1, -1)
+    return data
+
+def score_model(model: LogisticRegression, X_test, y_test):
+    score = model.score(X_test, y_test)
+    return score
+
+def train_with_kFold(X, y, n_splits):
+    model = LogisticRegression()
+    kf = KFold(n_splits=n_splits)
+    train_score = []
+    test_score = []
+    for train_index, test_index in kf.split(X):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        model.fit(X_train, y_train)
+        score_test = score_model(model, X_test, y_test)
+        score_train = score_model(model, X_train, y_train)
+        train_score.append(score_train)
+        test_score.append(score_test)
+    return model, train_score, test_score
+
+def get_confusion_matrix(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    cfs_matrix = confusion_matrix(y_test, y_pred)
+    return cfs_matrix
